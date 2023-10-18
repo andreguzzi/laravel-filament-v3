9f724e6e45cdd52c6f78b2a31a4af513617f219f
andreguzzi <andre_guzzi@hotmail.com>
2023-10-18 03:57:39 +0000

    migrations and models

---
+++ app/Models/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'email', 'phone', 'date_of_birth', 'address', 'zip_code', 'city'
+    ];
+
+}
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,6 @@
+<img
+    src="{{ asset('images/logo3.PNG') }}"
+    alt="André-Sistemas" 
+    class="h-16"
+/>
+    
